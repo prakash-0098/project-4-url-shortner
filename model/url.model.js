a225ec5f7b4cd05c84b3b9fa84385d978dc0b4dd
Prakash Sah <prakashsah10091@gmail.com>
2022-04-05 12:05:30 +0530

    done

--- model/url.model.js
+++ model/url.model.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
 
 const urlSchema = new mongoose.Schema({
     urlCode: {
@@ -11,7 +10,6 @@ const urlSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Long Url field is required !'],
         trim: true,
-        unique: true,
         match: [/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i, 'Enter the valid URL']
     },
     shortUrl: {
@@ -19,6 +17,5 @@ const urlSchema = new mongoose.Schema({
         trim: true
     }
 });
-uniqueValidator.defaults.message = "The longUrl already exist !"
-urlSchema.plugin(uniqueValidator);
+
 module.exports = mongoose.model('Url', urlSchema); 
